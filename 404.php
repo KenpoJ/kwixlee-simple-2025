b93b53b4d5205afc370b3b3ad1f8516fa543aab0
Jason Lyman <jason@lymannuthouose.com>
2025-04-07 21:53:33 -0700

    Modified button CTA language and the 404 page

--- 404.php
+++ 404.php
@@ -11,14 +11,16 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<div class="content-container">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'kwixlee_simple_2025' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Oops! Looks like this page didn&rsquo;t make the final cut.', 'kwixlee_simple_2025' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kwixlee_simple_2025' ); ?></p>
+				<p><?php esc_html_e( 'Maybe the link took a wrong turn at the script read-through?
+Try heading back to the homepage or check out the cast of links below to find your way!', 'kwixlee_simple_2025' ); ?></p>
 
 					<?php
 					get_search_form();
@@ -27,7 +29,7 @@ get_header();
 					?>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kwixlee_simple_2025' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( 'Categories', 'kwixlee_simple_2025' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories(
@@ -45,14 +47,15 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
-					$kwixlee_simple_2025_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kwixlee_simple_2025' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$kwixlee_simple_2025_archive_content" );
+					/*$kwixlee_simple_2025_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kwixlee_simple_2025' ), convert_smilies( ':)' ) ) . '</p>';
+					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$kwixlee_simple_2025_archive_content" );*/
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
+
 
 	</main><!-- #main -->
 
